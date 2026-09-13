4d18181f2ade1ca6568fb489856077d1c58346f3
htikkke <htikew574@gmail.com>
2026-09-13 17:17:50 +0630

    build PlanCard.

---
+++ src/PlanCard.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { FormContext } from "./App";
+
+export default function PlanCard({ icon, title, monthlyPrice, yearlyPrice }) {
+  const { isYearly } = useContext(FormContext);
+  return (
+    <div className="w-26 h-36 border border-custom-grey-500 p-3 rounded-lg flex flex-col items-start gap-8">
+      <img src={icon} alt={title} className="w-8 h-8" />
+      <div className="flex flex-col">
+        <p className="font-bold text-[15px] text-custom-blue-950">{title}</p>
+        <p className="font-normal text-xs text-custom-grey-500">
+          {isYearly ? `$${yearlyPrice}/yr` : `$${monthlyPrice}/mo`}
+        </p>
+        {isYearly && (
+          <p className="font-normal text-[11px] text-custom-blue-950 transition-all duration-200">
+            2 months free
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/SelectPlan.jsx
+++ src/SelectPlan.jsx
@@ -17,23 +17,7 @@ export default function SelectPlan() {
       {/* Select-Plan-Options */}
       <div className="flex gap-5">
         {/* Arcade */}
-        <div className="w-26 h-36 border border-custom-grey-500 p-3 rounded-lg flex flex-col items-start gap-8">
-          <img
-            src="/public/assets/images/icon-arcade.svg"
-            className="w-8 h-8"
-          />
-          <div className="flex flex-col">
-            <p className="font-bold text-[15px] text-custom-blue-950">Arcade</p>
-            <p className="font-normal text-xs text-custom-grey-500">
-              {isYearly ? "$90/yr" : "$9/mo"}
-            </p>
-            {isYearly && (
-              <p className="font-normal text-[11px] text-custom-blue-950 transition-all duration-200">
-                2 months free
-              </p>
-            )}
-          </div>
-        </div>
+
         {/* Advanced */}
         <div className="w-26 h-36 border border-custom-grey-500 p-3 rounded-lg flex flex-col items-start gap-8">
           <img
