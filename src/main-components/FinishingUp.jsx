a57f2f8d2fb888ab540aeb696888b336923907ab
htikkke <htikew574@gmail.com>
2026-09-20 17:21:10 +0630

    add onClick for back button in finishingUp.

--- src/main-components/FinishingUp.jsx
+++ src/main-components/FinishingUp.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { FormContext } from "../App";
+
 export default function FinishingUp() {
+  const { setCurrentStep } = useContext(FormContext);
+
   return (
     <div className="rounded-2xl p-8 w-full flex flex-col gap-8">
       {/* Finishing-Up-Header */}
@@ -43,10 +48,13 @@ export default function FinishingUp() {
       </div>
       {/* Back-Confirm-button */}
       <div className="flex justify-between mt-20">
-        <button className="rounded-lg text-custom-grey-500 text-sm font-bold">
+        <button
+          onClick={() => setCurrentStep((currentStep) => currentStep - 1)}
+          className="rounded-lg text-custom-grey-500 text-sm font-bold cursor-pointer"
+        >
           Go Back
         </button>
-        <button className="bg-custom-purple-600 rounded-lg text-custom-white py-2.5 px-5 text-sm">
+        <button className="bg-custom-purple-600 rounded-lg text-custom-white py-2.5 px-5 text-sm cursor-pointer">
           Confirm
         </button>
       </div>
