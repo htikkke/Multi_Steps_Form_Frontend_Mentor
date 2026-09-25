890156cb8f0115b65091a32b6ee4c9a22e02b6d7
htikkke <htikew574@gmail.com>
2026-09-25 16:32:46 +0630

    move addons data to data folder.

---
+++ src/data/ADDONS.jsx
@@ -0,0 +1,23 @@
+export const ADDONS = [
+  {
+    id: "Online service",
+    title: "Online service",
+    text: "Access to multiplayer games",
+    monthlyPrice: "+$1/mo",
+    yearlyPrice: "+$10/yr",
+  },
+  {
+    id: "Large storage",
+    title: "Larger storage",
+    text: "Extra 1TB of cloud save",
+    monthlyPrice: "+$2/mo",
+    yearlyPrice: "+$20/yr",
+  },
+  {
+    id: "Customizable Profile",
+    title: "Customizable Profile",
+    text: "Custom theme on your profile",
+    monthlyPrice: "+$2/mo",
+    yearlyPrice: "+$20/yr",
+  },
+];
--- src/main-components/AddOn.jsx
+++ src/main-components/AddOn.jsx
@@ -1,30 +1,7 @@
 import { useContext } from "react";
 import { FormContext } from "../App";
 import AddOnCard from "../side-components/AddOnCard";
-
-const ADDONS = [
-  {
-    id: "Online service",
-    title: "Online service",
-    text: "Access to multiplayer games",
-    monthlyPrice: "+$1/mo",
-    yearlyPrice: "+$10/yr",
-  },
-  {
-    id: "Large storage",
-    title: "Larger storage",
-    text: "Extra 1TB of cloud save",
-    monthlyPrice: "+$2/mo",
-    yearlyPrice: "+$20/yr",
-  },
-  {
-    id: "Customizable Profile",
-    title: "Customizable Profile",
-    text: "Custom theme on your profile",
-    monthlyPrice: "+$2/mo",
-    yearlyPrice: "+$20/yr",
-  },
-];
+import { ADDONS } from "../data/ADDONS";
 
 export default function AddOn() {
   const { setCurrentStep } = useContext(FormContext);
