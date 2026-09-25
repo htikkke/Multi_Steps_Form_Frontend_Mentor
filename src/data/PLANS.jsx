7cf9c8100f6e8624180afeed3ddf6b5db7af3a9c
htikkke <htikew574@gmail.com>
2026-09-25 16:34:09 +0630

    move plans data to data folder.

---
+++ src/data/PLANS.jsx
@@ -0,0 +1,23 @@
+export const PLANS = [
+  {
+    id: "arcade",
+    title: "Arcade",
+    monthlyPrice: 9,
+    yearlyPrice: 90,
+    icon: "/assets/images/icon-arcade.svg",
+  },
+  {
+    id: "advanced",
+    title: "Advanced",
+    monthlyPrice: 12,
+    yearlyPrice: 120,
+    icon: "/assets/images/icon-advanced.svg",
+  },
+  {
+    id: "pro",
+    title: "Pro",
+    monthlyPrice: 15,
+    yearlyPrice: 150,
+    icon: "/assets/images/icon-pro.svg",
+  },
+];
--- src/main-components/SelectPlan.jsx
+++ src/main-components/SelectPlan.jsx
@@ -1,30 +1,7 @@
 import { useContext } from "react";
 import PlanCard from "../side-components/PlanCard";
 import { FormContext } from "../App";
-
-const PLANS = [
-  {
-    id: "arcade",
-    title: "Arcade",
-    monthlyPrice: 9,
-    yearlyPrice: 90,
-    icon: "/assets/images/icon-arcade.svg",
-  },
-  {
-    id: "advanced",
-    title: "Advanced",
-    monthlyPrice: 12,
-    yearlyPrice: 120,
-    icon: "/assets/images/icon-advanced.svg",
-  },
-  {
-    id: "pro",
-    title: "Pro",
-    monthlyPrice: 15,
-    yearlyPrice: 150,
-    icon: "/assets/images/icon-pro.svg",
-  },
-];
+import { PLANS } from "../data/PLANS";
 
 export default function SelectPlan() {
   const { isYearly, setIsYearly, setCurrentStep } = useContext(FormContext);
