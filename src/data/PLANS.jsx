91c2b34677b94c3a2163261845755b41f466f872
htikkke <htikew574@gmail.com>
2026-09-26 16:33:32 +0630

    change the photo paths.

--- src/data/PLANS.jsx
+++ src/data/PLANS.jsx
@@ -4,20 +4,20 @@ export const PLANS = [
     title: "Arcade",
     monthlyPrice: 9,
     yearlyPrice: 90,
-    icon: "/assets/images/icon-arcade.svg",
+    icon: "./assets/images/icon-arcade.svg",
   },
   {
     id: "advanced",
     title: "Advanced",
     monthlyPrice: 12,
     yearlyPrice: 120,
-    icon: "/assets/images/icon-advanced.svg",
+    icon: "./assets/images/icon-advanced.svg",
   },
   {
     id: "pro",
     title: "Pro",
     monthlyPrice: 15,
     yearlyPrice: 150,
-    icon: "/assets/images/icon-pro.svg",
+    icon: "./assets/images/icon-pro.svg",
   },
 ];
--- src/main-components/ThankU.jsx
+++ src/main-components/ThankU.jsx
@@ -2,7 +2,7 @@ export default function ThankU() {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center text-center px-18 py-8">
       <div className="flex flex-col items-center justify-center gap-5 text-center">
-        <img src="/assets/images/icon-thank-you.svg" />
+        <img src="./assets/images/icon-thank-you.svg" />
         <p className="font-bold text-custom-blue-950 text-2xl">Thank you!</p>
         <p className="font-normal text-custom-grey-500 text-sm">
           Thanks for confirming your subscription! We hope you have fun using
